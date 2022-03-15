413043ea5ee62b45c9f38cdc63d87947cd886d81
Min ho Kim <minho42@gmail.com>
2022-03-16 09:14:03 +1100

    init

---
+++ components/Navbar.js
@@ -0,0 +1,30 @@
+import Link from "next/link";
+import { useRouter } from "next/router";
+
+export function Navbar() {
+  const router = useRouter();
+
+  return (
+    <div className="flex w-full border-b border-gray-300  items-center justify-center space-x-6 h-12 text-xl mb-3">
+      <Link href="/">
+        <div
+          className={`rounded-md px-3 py-2 hover:bg-pink-100 cursor-pointer
+        ${router.pathname == "/" ? "font-semibold" : ""}
+        `}
+        >
+          😷 Covid swab list
+        </div>
+      </Link>
+
+      <Link href="/about">
+        <div
+          className={`rounded-md px-3 py-2 hover:bg-pink-100 cursor-pointer
+        ${router.pathname == "/about" ? "font-semibold" : ""}
+        `}
+        >
+          About
+        </div>
+      </Link>
+    </div>
+  );
+}
---
+++ pages/about.js
@@ -0,0 +1,37 @@
+export default function About() {
+  return (
+    <div className="space-y-3">
+      <div>
+        <div className="text-xl font-semibold">Swab days</div>
+        <ul className="list-disc">
+          <li>Calculated from admission date</li>
+          <li>Swab are due on: day [0, 2, 5, 8]</li>
+        </ul>
+      </div>
+
+      <div>
+        <div className="text-xl font-semibold">How to use</div>
+        <ul className="list-decimal">
+          <li>Open PowerChart</li>
+          <li>Go to patient list</li>
+          <li>Select all (Ctrl + A)</li>
+          <li>Copy (Ctrl + C)</li>
+          <li>Open this website</li>
+          <li>Go to list</li>
+          <li>Select column numbers for each fields</li>
+          <li>Click Make button</li>
+          <li>Allow permission to access clipboard</li>
+        </ul>
+      </div>
+
+      <div>
+        <div className="text-xl font-semibold">Security</div>
+        <ul className="list-disc">
+          <li>This website doesn't have a server</li>
+          <li>Nothing is saved anywhere</li>
+          <li>99.99% secure</li>
+        </ul>
+      </div>
+    </div>
+  );
+}
