a3eb2b98afbcc7050117bbd59c9111226bd72918
Min ho Kim <minho42@gmail.com>
2022-03-16 15:26:36 +1100

    trying to fix eslint warnings: passHref

--- components/Navbar.js
+++ components/Navbar.js
@@ -6,7 +6,7 @@ export function Navbar() {
 
   return (
     <div className="flex w-full border-b border-gray-300  items-center justify-center space-x-6 h-12 text-xl mb-3">
-      <Link href="/">
+      <Link href="/" passHref>
         <div
           className={`rounded-md px-3 py-2 hover:bg-pink-100 cursor-pointer
         ${router.pathname == "/" ? "font-semibold" : ""}
@@ -16,7 +16,7 @@ export function Navbar() {
         </div>
       </Link>
 
-      <Link href="/about">
+      <Link href="/about" passHref>
         <div
           className={`rounded-md px-3 py-2 hover:bg-pink-100 cursor-pointer
         ${router.pathname == "/about" ? "font-semibold" : ""}
